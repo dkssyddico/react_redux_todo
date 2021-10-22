456ed9c1923c1f3ff0be83c0e51524bd95d182e0
dkssyddico <dkssyddico@naver.com>
2021-10-22 18:33:57 +0900

    divide Main component into Form and Lists

---
+++ src/components/AddTodoForm.js
@@ -0,0 +1,37 @@
+import React, { memo, useState, useCallback } from 'react';
+import { connect } from 'react-redux';
+import { todoActionCreators } from '../modules/todoReducer';
+
+const AddTodoForm = ({ addToDo }) => {
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === '') {
+      alert('내용을 입력해주세요!');
+    } else {
+      addToDo(text);
+      setText('');
+    }
+  };
+
+  const onChange = useCallback((e) => {
+    const { value } = e.target;
+    setText(value);
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={text} onChange={onChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return { addToDo: (text) => dispatch(todoActionCreators.add(text)) };
+};
+
+export default connect(null, mapDispatchToProps)(AddTodoForm);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { todoActionCreators } from '../modules/todoReducer';
+import AddTodoForm from './AddTodoForm';
 import Todo from './Todo';
 
-function Main({ todos, addToDo }) {
-  // console.log(todos);
-  const [text, setText] = useState('');
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (text.trim() === '') {
-      alert('내용을 입력해주세요!');
-    }
-    addToDo(text);
-    setText('');
-  };
-
-  const onChange = (e) => {
-    const { value } = e.target;
-    setText(value);
-  };
-
+function Main({ todos }) {
   return (
     <div>
       <h1>Write a to do here!</h1>
-      <form onSubmit={onSubmit}>
-        <input type='text' value={text} onChange={onChange} />
-        <button>Submit</button>
-      </form>
+      <AddTodoForm />
       <ul>
         {todos.map((todo) => (
           <Todo key={todo.id} {...todo} />
@@ -37,12 +17,8 @@ function Main({ todos, addToDo }) {
   );
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   return { todos: state };
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return { addToDo: (text) => dispatch(todoActionCreators.add(text)) };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(mapStateToProps, null)(Main);
