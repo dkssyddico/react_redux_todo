1a5a5770fce8262220e7cf30fca46fde394c5b07
dkssyddico <dkssyddico@naver.com>
2021-10-23 21:55:25 +0900

    modify todoHeader

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 function TodoHeader({ todos }) {
   const incompleteTodos = todos.filter((todo) => todo.complete === false);
+  const today = new Date();
+  const dateString = today.toLocaleDateString('ko-KR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const day = today.toLocaleDateString('ko-KR', {
+    weekday: 'long',
+  });
+
   return (
     <header>
       <h1>Todo List</h1>
+      <p>{dateString}</p>
+      <p>{day}</p>
       <span>{incompleteTodos.length}</span>
     </header>
   );
